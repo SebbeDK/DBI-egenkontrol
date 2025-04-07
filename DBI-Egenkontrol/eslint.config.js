5efc007d1fb9b102a7d1ef1c0de17200e69e910b
SebbeDK <sebastianmadsen99@gmail.com>
2025-04-07 12:06:34 +0200

    ESLint implementeret
    ESLint er blevet installeret og konfigureret med en håndfuld regler.

--- DBI-Egenkontrol/eslint.config.js
+++ DBI-Egenkontrol/eslint.config.js
@@ -1,41 +1,25 @@
-import js from '@eslint/js'
-import pluginVue from 'eslint-plugin-vue'
-import globals from 'globals'
-import pluginVitest from '@vitest/eslint-plugin'
-import pluginCypress from 'eslint-plugin-cypress/flat'
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import js from '@eslint/js';
+import pluginVue from 'eslint-plugin-vue';
 
-export default [
-  {
-    name: 'app/files-to-lint',
-    files: ['**/*.{js,mjs,jsx,vue}'],
-  },
 
-  {
-    name: 'app/files-to-ignore',
-    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+export default defineConfig([
+  { files: ['**/*.{js,mjs,cjs,vue}'] },
+  { files: ['**/*.{js,mjs,cjs,vue}'], languageOptions: { globals: globals.browser } },
+  { files: ['**/*.{js,mjs,cjs,vue}'], plugins: { js }, extends: ['js/recommended'] },
+  pluginVue.configs['flat/essential'],
+  { rules: {
+    'comma-dangle': ['error', 'always-multiline'],
+    'eqeqeq': ['error', 'always'],
+    'indent': ['error', 2],
+    'no-eval': ['error'],
+    'no-trailing-spaces': ['error'],
+    'no-unused-vars': ['error'],
+    'no-var': ['error'],
+    'prefer-const': ['error'],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'always'],
   },
-
-  {
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-      },
-    },
   },
-
-  js.configs.recommended,
-  ...pluginVue.configs['flat/essential'],
-  
-  {
-    ...pluginVitest.configs.recommended,
-    files: ['src/**/__tests__/*'],
-  },
-  
-  {
-    ...pluginCypress.configs.recommended,
-    files: [
-      'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}',
-      'cypress/support/**/*.{js,ts,jsx,tsx}'
-    ],
-  },
-]
+]);
