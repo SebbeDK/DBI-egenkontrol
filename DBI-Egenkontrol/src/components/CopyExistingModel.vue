035e38be0a1bb90e5b0cd47aefa8e1c68618037a
Carina <54623517+chvessel@users.noreply.github.com>
2025-05-05 13:57:22 +0200

    fået lavet routing
    fået lavet routing fra "tilgængelige skabeloner" til "lav skema"

--- DBI-Egenkontrol/src/components/CopyExistingModel.vue
+++ DBI-Egenkontrol/src/components/CopyExistingModel.vue
@@ -1,8 +1,15 @@
 <script setup>
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+function goToSkabelon() {
+  router.push('/skemaer/skema/opret')
+}
 </script>
 
 <template>
-    <div class="container">
+    <div class="container" @click="goToSkabelon()">
         <h3>Kopiér fra en tilgængelig skabelon</h3>
         <p>Kopiér et eksisterende skema og tilpas <br> det som du ønsker.</p>
     </div>
@@ -15,6 +22,7 @@ border-radius: 10px;
 width: 20rem;
 padding: 1rem;
 padding-left: 2rem;
+cursor: pointer;
 }
 
 h3{
